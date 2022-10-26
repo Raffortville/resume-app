04698f200c58a9f9b09dde65883d0ab3f267f6a8
Raffortville <raffihaycan@gmail.com>
2022-10-26 10:02:46 +0400

    add user store actions

--- client/app/src/store/user/userStore.ts
+++ client/app/src/store/user/userStore.ts
@@ -1,8 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { RootState, store } from '..';
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { fireBaseAuth } from '../../services';
+import { RootState } from '..';
 import type { IUser, UserType } from '../types';
+import { displayAlert } from '../alert/alertStore';
 
 export const initialState: { user: UserType } = {
 	user: null,
@@ -28,24 +32,161 @@ const userSlice = createSlice({
 
 export const { setUser, resetUser, updateUser } = userSlice.actions;
 
-export const signUp = async (payload: {
-	email: string;
-	password: string;
-}): Promise<any> => {
+export const signUp = async (payload: IUser): Promise<void> => {
 	const { email, password } = payload;
+
+	if (!password) {
+		displayAlert({
+			payload: {
+				message: 'Le mot de passe est manquant',
+				type: 'error',
+			},
+		});
+		return;
+	}
+
 	try {
-		const user = await createUserWithEmailAndPassword(
+		const { user } = await createUserWithEmailAndPassword(
 			fireBaseAuth,
 			email,
 			password
 		);
-		setUser({ email: 'tete', password: 'tetete' });
-		return user;
+
+		if (!user.email || user.email === null) {
+			displayAlert({
+				payload: {
+					message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+					type: 'error',
+				},
+			});
+			return;
+		}
+
+		logIn({ payload: { email: user.email, password }, isFirstLogin: true });
 	} catch (err) {
-		return err;
+		displayAlert({
+			payload: {
+				message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+				type: 'error',
+			},
+		});
+	}
+};
+
+export const logIn = async ({
+	payload,
+	isFirstLogin,
+}: {
+	payload: IUser;
+	isFirstLogin: boolean;
+}): Promise<void> => {
+	const { password, email } = payload;
+
+	if (!password) {
+		throw new Error('Le mot de passe est manquant');
+	}
+
+	try {
+		const { user } = await signInWithEmailAndPassword(
+			fireBaseAuth,
+			email,
+			password
+		);
+
+		if (!user.email || user.email === null) {
+			displayAlert({
+				payload: {
+					message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+					type: 'error',
+				},
+			});
+			return;
+		}
+
+		isFirstLogin
+			? saveUserToDb({ email: user.email })
+			: getUser({ email: user.email });
+	} catch (error) {
+		displayAlert({
+			payload: {
+				message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+				type: 'error',
+			},
+		});
+	}
+};
+
+export const saveUserToDb = async (payload: IUser): Promise<void> => {
+	const { email } = payload;
+
+	try {
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email }),
+		});
+
+		if (response.status !== 200) {
+			displayAlert({
+				payload: {
+					message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+					type: 'error',
+				},
+			});
+			return;
+		}
+
+		const data = await response.json();
+		setUser(data);
+	} catch (error) {
+		displayAlert({
+			payload: {
+				message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+				type: 'error',
+			},
+		});
+	}
+};
+
+export const getUser = async (payload: IUser): Promise<void> => {
+	const { email } = payload;
+	try {
+		const response = await fetch(
+			`${process.env.REACT_APP_API_URL}/user/loadUser`,
+			{
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ email }),
+			}
+		);
+
+		if (response.status !== 200) {
+			displayAlert({
+				payload: {
+					message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+					type: 'error',
+				},
+			});
+			return;
+		}
+
+		const data = await response.json();
+		setUser(data);
+	} catch (error) {
+		displayAlert({
+			payload: {
+				message: 'Erreur lors de votre connexion, veuillez essayer plus tard',
+				type: 'error',
+			},
+		});
 	}
 };
 
 export const userSelector = (state: RootState) => state.userReducer.user;
-
 export const userReducer = userSlice.reducer;
