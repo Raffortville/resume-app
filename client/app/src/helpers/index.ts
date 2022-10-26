a99b25f985ef2596670e203ecb781aa3462a826e
Raffortville <raffihaycan@gmail.com>
2022-10-26 11:02:28 +0400

    add helpers functions for client front

---
+++ client/app/src/helpers/index.ts
@@ -0,0 +1,31 @@
+export const checkIsValidInputFormat = (
+	input: string,
+	type: string
+): boolean => {
+	switch (type) {
+		case 'email':
+			const pattern = new RegExp(/.+@.+\..+/i);
+			return Boolean(!pattern.test(input));
+
+		case 'password': {
+			return Boolean(input.length > 6);
+		}
+
+		case 'text': {
+			const letters = /^[A-Za-z]+$/;
+			return Boolean(input.match(letters));
+		}
+
+		case 'number': {
+			const numbers = /^[0-9]+$/;
+			return Boolean(input.match(numbers));
+		}
+
+		default:
+			return false;
+	}
+};
+
+export const isStringEmpty = (input: string): boolean => {
+	return input === undefined || input === null || input === '';
+};
