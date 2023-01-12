39db03a627bd4ce46397a010fe8a8e5d472a7d97
Raffortville <raffihaycan@gmail.com>
2023-01-12 17:23:55 +0400

    install react pdf

--- client/app/src/components/pages/resume/view/ResumeViewContainer.tsx
+++ client/app/src/components/pages/resume/view/ResumeViewContainer.tsx
@@ -1,5 +1,19 @@
+import { PDFViewer, View, Page, Text, Document } from '@react-pdf/renderer';
 import React from 'react';
 
 export const ResumeViewContainer: React.FC = () => {
-	return <div>Resume view</div>;
+	return (
+		<PDFViewer>
+			<Document>
+				<Page size='A4'>
+					<View>
+						<Text>Section #1</Text>
+					</View>
+					<View>
+						<Text>Section #2</Text>
+					</View>
+				</Page>
+			</Document>
+		</PDFViewer>
+	);
 };
